1f257c76df6c76f5d9a8e0fcc60424d6abec6f25
万发东 <fadongwan@163.com>
2017-01-27 23:44:56 +0800

    remove unneccessray handler

--- hello/main.go
+++ hello/main.go
@@ -7,16 +7,16 @@ import (
 	"io/ioutil"
 	"net/http"
 	"log"
+	"time"
 )
-
 
 var (
-	peers_addrs = []string{"http://127.0.0.1:8001", "http://127.0.0.1:8002", "http://127.0.0.1:8003"}
+	peers_addrs = []string{"http://127.0.0.1:8001", "http://127.0.0.1:8002"}
 )
 
-func test() {
+func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("\r\n Usage local_addr \t\n local_addr must in(8001, 8002, 8003)\r\n")
+		fmt.Println("\r\n Usage local_addr \t\n local_addr must in(8001, 8002)\r\n")
 		os.Exit(1)
 	}
 	local_addr := os.Args[1]
@@ -24,9 +24,9 @@ func test() {
 	peers.Set(peers_addrs...)
 
 	// 初始化Group集群（逻辑，或者说是命名空间）
-	var image_cache = groupcache.NewGroup("imagegroup", 8 << 30, groupcache.GetterFunc(
+	var image_cache = groupcache.NewGroup("info", 8 << 30, groupcache.GetterFunc(
 		func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
-			result, err := ioutil.ReadFile(key)// []byte
+			result, err := ioutil.ReadFile("data/" + key)// []byte
 			if err != nil {
 				fmt.Printf("read file error %s.\n", err.Error())
 				return nil
@@ -35,12 +35,12 @@ func test() {
 			dest.SetBytes(result)
 			return nil
 		}))
-	http.HandleFunc("/image", func(rw http.ResponseWriter, r *http.Request) {
-		var data []byte
-		k := r.URL.Query().Get("id")
-		fmt.Printf("user get %s from groupcache\n", k)
-		image_cache.Get(nil, k, groupcache.AllocatingByteSliceSink(&data))
-		rw.Write([]byte(data))
-	})
+
+	go func() {
+		ticker := time.NewTicker(time.Minute)
+		for range ticker.C {
+			fmt.Println(image_cache.Stats.Gets)
+		}
+	}()
 	log.Fatal(http.ListenAndServe("127.0.0.1:" + local_addr, nil))
 }
